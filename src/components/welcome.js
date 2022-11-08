47289906d62839f68db59dcfa8c16d1084c56911
Angelica Garcia <anyegar36@gmail.com>
2022-11-08 16:37:43 -0500

    Add component welcome

---
+++ src/components/welcome.js
@@ -0,0 +1,14 @@
+export const welcome = () => {
+    const index = `<section class="welcome">
+    <section class="welcome__container">
+      <img src="assets/img/logo.svg" alt="logo" class="welcome__img-container">
+      <h1 class="welcome__title-container">Bienvenidos a FoodTrack </h1>
+      <p class="welcome__description-container">Con esta App podrás compartir tus comida favorita, hacer review, buscar y localizar platillos exquisitos en tu país</p>
+      <div class="welcome__buttons-container">
+        <a href="#" class="welcome__login-buttons">Iniciar Sesión</a>
+        <a href="#" class="welcome__signIn-buttons">Registrarse</a>
+      </div>
+    </section>
+  </section>`
+  return index;
+}
