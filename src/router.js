ce1e71c3773ac8a93b77910c472b9c1ed5ab5981
Natioterom <natioterom@gmail.com>
2022-11-08 22:26:36 -0300

    SPA

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ const PATHS = {
           <p class="welcome__description-container">Con esta App podrás compartir tus comida favorita, hacer review, buscar y localizar platillos exquisitos en tu país</p>
           <div class="welcome__buttons-container">
           <a href="login" class="welcome__login-buttons">Iniciar Sesión</a>
-          <a href="#" class="welcome__signIn-buttons">Registrarse</a>
+          <a href="registro" class="welcome__signIn-buttons">Registrarse</a>
         </div>
         </section>
       </section>`
@@ -20,18 +20,29 @@ login:{
     <section class="login__container">
      <img src="assets/img/logo.svg" alt="logo" class="welcome__img-container"/>
       <div class= "login__datos-container">
-    <input class=login__datos-set placeholder="Nombre"></input>
     <input class=login__datos-set placeholder="e-mail."></input> 
     <input class=login__datos-set placeholder="Contraseña"></input> 
-    <input class=login__datos-set placeholder="País"></input>     
     </div>
     <a href="#" class="login-button">Iniciar Sesión</a>
     </section>
   </section>`
         },
 
-signIn:{
-    path:'/resgistro',
+registro:{
+    path:'/registro',
+    template:
+    `<section class="login">
+    <section class="login__container">
+     <img src="assets/img/logo.svg" alt="logo" class="welcome__img-container"/>
+      <div class= "login__datos-container">
+    <input class=login__datos-set placeholder="Nombre"></input>
+    <input class=login__datos-set placeholder="e-mail."></input> 
+    <input class=login__datos-set placeholder="Contraseña"></input> 
+    <input class=login__datos-set placeholder="País"></input>     
+    </div>
+    <a href="#" class="login-button">Registar</a>
+    </section>
+  </section>`
     }
 }
 class Router {
