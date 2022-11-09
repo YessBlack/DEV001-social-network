2b8906413f66ab386f083072df36cb88c2f5e10b
Angelica Garcia <anyegar36@gmail.com>
2022-11-09 13:42:17 -0500

    Change SPA

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-// Este es el punto de entrada de tu aplicacion
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyChbY78bZsBles4iIY64FCX7dys64lzrVA',
+  authDomain: 'foodtrack-6348d.firebaseapp.com',
+  projectId: 'foodtrack-6348d',
+  storageBucket: 'foodtrack-6348d.appspot.com',
+  messagingSenderId: '128983646489',
+  appId: '1:128983646489:web:098956b4d4e31695bf8588',
+};
 
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
