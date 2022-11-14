31aa50b8396000d421f40c187125a0a7bcf376d4
Angelica Garcia <anyegar36@gmail.com>
2022-11-14 09:35:27 -0500

    change component login

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,19 @@
 export const login = () => {
-  const pageLogin = `<section class="login">
-    <section class="login__container">
-      <img src="assets/img/logo.svg" alt="logo" class="welcome__img-container"/>
-      <div class= "login__datos-container">
-    <input class=login__datos-set placeholder="email" id="emailLogin"></input> 
-    <input class=login__datos-set placeholder="Contraseña" id="passwordLogin"></input> 
-    </div>
-    <a class="login-button"  id="login" >Iniciar Sesión</a>
-    <a href='#home' class="welcome-button" id="welcom">Home</a>
+  const pageLogin = `<section class="containerAuth container">
+    <section class="containerAuth__content">
+      <h1 class="containerAuth__title-content">Inicia sesion</h1>
+      <div class= "containerAuth__register-content">
+        <form class="containerAuth__form-register" id="formRegister">
+          <input class="containerAuth__email-form" placeholder="email" id="emailLogin" name="email">
+          <input class="containerAuth__password-form" placeholder="Contraseña" id="passwordLogin" name="password" type="password">
+          <h3 class="lines-effect">OR</h3>
+          <a href="#" id="loginGoogle" class="containerAuth__button-google">
+            <img src="assets/img/google.png" alt="logo de google">
+            Login con Google
+          </a>
+          <button class="containerAuth__login-button" id="login">Ingresar</button>
+        </form>
+      </div>
     </section>
   </section>`;
   return pageLogin;
