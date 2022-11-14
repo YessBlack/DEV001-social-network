114bebe99349494b789421e7e8285b152f6d5d3c
Angelica Garcia <anyegar36@gmail.com>
2022-11-14 09:35:55 -0500

    change componente registero

--- src/components/registro.js
+++ src/components/registro.js
@@ -1,13 +1,21 @@
 export const registro = () => {
-  const pageRegistro = `<section class="login">
-    <section class="login__container">
-    <img src="assets/img/logo.svg" alt="logo" class="welcome__img-container"/>
-      <div class= "login__datos-container">
-    <input class=login__datos-set placeholder="email" id="email"></input> 
-    <input class=login__datos-set placeholder="Contraseña" id="password"></input> 
-    </div>
-    <a href="#" class="login-button" id="registro">Registar</a>
-    <a href="#home" class="welcome-button" id="welcom">Home</a>
+  const pageRegistro = `<section class="containerAuth container">
+    <section class="containerAuth__content">
+      <h1 class="containerAuth__title-content">Registrate</h1>
+      <div class= "containerAuth__register-content">
+        <form class="containerAuth__form-register" id="formRegister">
+          <input type="text" class="containerAuth__name-form" id="name" placeholder="name" name="name">
+          <input class="containerAuth__email-form" placeholder="email" id="email" name="email">
+          <input class="containerAuth__password-form" placeholder="Contraseña" id="password" name="password" type="password">
+          <input type="text" class="containerAuth__country-form" placeholder="País" id="country" name="country"><br>
+          <h3 class="lines-effect">OR</h3>
+          <a href="#" id="registerGoogle" class="containerAuth__button-google">
+            <img src="assets/img/google.png" alt="logo de google">
+            Registrarse con Google
+          </a>
+          <button class="containerAuth__login-button" id="registro">Registar</button>
+        </form>
+      </div>
     </section>
   </section>`;
   return pageRegistro;
