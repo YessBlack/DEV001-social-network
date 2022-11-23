6240b03546ba0f870f7833fa6c6abbab215a3934
Angelica Garcia <anyegar36@gmail.com>
2022-11-23 17:31:36 -0500

    Change test components registro

--- test/components.spec.js
+++ test/components.spec.js
@@ -1,4 +1,3 @@
-import { beforeAuthStateChanged } from 'firebase/auth';
 import { registro, eventsRegistro } from '../src/components/Registro.js';
 import { welcome } from '../src/components/Welcome.js';
 // eslint-disable-next-line import/named
@@ -29,15 +28,13 @@ describe('welcome', () => {
   });
 
   it('Deberia ser un enlace que redireccione a #login', () => {
-    const div = document.createElement('div');
-    div.innerHTML = welcome();
-    expect(div.querySelector('#login').getAttribute('href')).toBe('#login');
+    document.body.innerHTML = welcome();
+    expect(document.querySelector('#login').getAttribute('href')).toBe('#login');
   });
 
   it('Deberia ser un enlace que redireccione a #registrar', () => {
-    const div = document.createElement('div');
-    div.innerHTML = welcome();
-    expect(div.querySelector('#register').getAttribute('href')).toBe('#registrar');
+    document.body.innerHTML = welcome();
+    expect(document.querySelector('#register').getAttribute('href')).toBe('#registrar');
   });
 });
 describe('registro', () => {
@@ -51,27 +48,26 @@ describe('registro', () => {
 });
 
 describe('eventsRegistro', () => {
+  const $ = (selector) => document.querySelector(selector);
+
+  beforeEach(() => {
+    document.body.innerHTML = registro();
+    eventsRegistro();
+  });
+
   it('Debería ser una función', () => {
     expect(typeof eventsRegistro).toBe('function');
   });
 
   it('$ Deberia ser una funcion para selectores de DOM', () => {
-    const $ = (selector) => document.querySelector(selector);
-    document.body.innerHTML = registro();
     expect(typeof $).toBe('function');
     expect($('.error-mail').getAttribute('class')).toBe('error-mail');
   });
 
   it('Deberia disparar un evento submit para registrar con email y password', (done) => {
     createUserWithEmailAndPassword.mockImplementation(() => Promise.resolve({}));
-    document.body.innerHTML = '';
-    const div = document.createElement('div');
-    div.innerHTML = registro();
-    document.body.appendChild(div);
-    eventsRegistro();
-    const form = document.querySelector('#formRegister');
     const event = new Event('submit');
-    form.dispatchEvent(event);
+    $('#formRegister').dispatchEvent(event);
     expect(event.type).toBe('submit');
     window.addEventListener('hashchange', () => {
       expect(window.location.hash).toBe('#timeline');
@@ -80,22 +76,16 @@ describe('eventsRegistro', () => {
   });
 
   it('Deberia disparar un evento click para registrar con Google', (done) => {
-    document.body.innerHTML = '';
     getAuth.mockImplementationOnce(() => 'hola');
     signInWithPopup.mockImplementationOnce((auth) => {
       expect(auth).toBe('hola');
       expect(googleAuthProviderHelper).toHaveBeenCalled();
       return Promise.resolve();
     });
-    const div = document.createElement('div');
-    div.innerHTML = registro();
-    document.body.appendChild(div);
-    eventsRegistro();
     window.addEventListener('hashchange', () => {
       expect(window.location.hash).toBe('#timeline');
       done();
     });
-    const btn = document.querySelector('#registerGoogle');
-    btn.dispatchEvent(new Event('click'));
+    $('#registerGoogle').dispatchEvent(new Event('click'));
   });
 });
